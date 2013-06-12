e56b9bffd479a10088002897afac0b483c2a3a6b
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-06-12 15:41:48 +1000

    Updated Code Chicken mods

--- AnorakPack.py
+++ AnorakPack.py
@@ -12,13 +12,13 @@ def Define():
 	Anorak.universal_coremods += """
 @[1.5.2]TreeCapitator.Forge.1.5.2.r01.Uni.CoreMod.jar
 Chemcraft Core 1.3.0.jar
-CodeChickenCore_0.8.6.12.jar
+CodeChickenCore 0.8.7.jar
 CoFHCore-1.5.2.4.jar
 denLib-1.5.2-3.0.14.jar
 Galacticraft-1.5.2-a0.1.35.300.jar
 immibis-microblocks-55.0.1.jar
 @MultiMine_1.5.2.jar
-NotEnoughItems_1.5.2.21.jar
+NotEnoughItems_1.5.2.28.jar
 PowerCrystalsCore-1.1.6-107.jar
 @StalkerCreepers_1.5.2.jar
 Snowfall 1.2.1.jar
@@ -35,7 +35,7 @@ Atomic_Science_v0.5.3.51.jar
 BaMsDoubledoor_v2.0_for_1.5.2_ForgeMod.zip
 BiblioCraft[v1.2.3].zip
 Carpenter's_Slope_v1.25_-_MC_1.5+.zip
-ChickenChunks_1.3.2.10.jar
+ChickenChunks_1.3.2.12.jar
 chisel-1.5.2-1.4.4.jar
 CompactWindmills+v.1.0.1.3.jar
 ComplexMachines_v0.3.3.92.jar
