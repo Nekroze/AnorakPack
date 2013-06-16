932097cbe5e108ca7d8c7f805e0a22ccc5bc603c
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-06-16 18:06:03 +1000

    Removed Tinkers Recovery

--- AnorakPack.py
+++ AnorakPack.py
@@ -77,7 +77,6 @@ Improved_Mob_Spawn_1.4.0.zip
 InfernalMobs_1.5.2.zip
 invasion_mod_0.11.7.zip
 ironchest-universal-1.5.2-5.2.6.425.zip
-KBI Tinkers Construct Recovery Addon 1.2.1.zip
 LogisticsPipes-MC1.5.2-0.7.3.dev.524.jar
 Mekanism-v5.5.6.82.jar
 MekanismGenerators-v5.5.6.82.jar
