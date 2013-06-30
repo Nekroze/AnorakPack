98addcfc1a0233f5a6fbbe3d95cf8cb275c7b0be
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-07-01 02:09:28 +1000

    Removed a few mods outright and moved lite version to full to create a newer lighter lite version.

--- AnorakPack.py
+++ AnorakPack.py
@@ -19,53 +19,35 @@ immibis-microblocks-55.0.5.jar
 @MultiMine_1.5.2.jar
 NotEnoughItems 1.5.2.28.jar
 PowerCrystalsCore-1.1.6-107.jar
-@StalkerCreepers_1.5.2.jar
-@Snowfall 1.2.1.jar
 """
 
     Anorak.universal_mods += """
-1.5.2_Jammy_Furniture_Mod_V4.4.zip
-@1.5.2BetterBreeding.zip
+@1.5.2_Jammy_Furniture_Mod_V4.4.zip
 appeng-rv11-b.zip
 Atomic_Science_v0.6.0.73.jar
 BaMsDoubledoor_v2.0_for_1.5.2_ForgeMod.zip
-BiblioCraft[v1.3.1].zip
+@BiblioCraft[v1.3.1].zip
 buildcraft-A-3.7.1.jar
 buildcraft-Z-additional-buildcraft-objects-1.0.4.150.jar
 Carpenter's Blocks v1.44 - MC 1.5+.zip
 ChickenChunks 1.3.2.12.jar
-chisel-1.5.2-1.4.4.jar
+@chisel-1.5.2-1.4.4.jar
 ComplexMachines_v0.3.3.119.jar
 ComputerCraft1.53.zip
 ConfigMod_for_MC_v1.5.2.zip
 @coral-reef-universal-1.5.2-r2.zip
-@CoroAI_for_MC_v1.5.2.zip
 @CraftHeraldry 1.0.1.zip
-@Defense-1.4.1.zip
 DenPipes-1.5.2-2.0.7.jar
 DenPipes-Forestry-1.5.2-1.0.3.jar
-@DimensionalDoors-1.5.2R1.4.0RC1-144.zip
-@Dungeon Mobs v2.6.0.zip
-@EmasherCore-1.6.0.zip
 ElectricExpansion_v2.3.0.51.jar
-@Electrodynamics-Alpha-0.1.2.jar
 EnderStorage 1.4.2.12.jar
-@enhanced-portals_2.1.2.jar
 extra-bees-1.6-pre14b.jar
 ExtraCells-universal-1.2.2b.jar
 Factorization-0.7.37.jar
 forestry-A-2.2.8.3.jar
 FlatBedrock-1.1.1-32.jar
-@GasCraft-1.6.1.zip
 GateCopy-1.5.2-3.0.2.jar
 GregsLighting-1.8.1-mc1.5.1.jar
-@growthcraft-apples-1.5.2-4.0.zip
-@growthcraft-core-1.5.2-1.0.zip
-@growthcraft-core-booze-1.5.2-1.1.zip
-@growthcraft-fishnet-1.5.2-4.1.zip
-@growthcraft-flowers-1.5.2-5.2.zip
-@growthcraft-grapes-1.5.2-4.0.zip
-@growthcraft-hops-1.5.2-2.0.zip
 @HangableMaps_v1.5.2_1_MC1.5.2.zip
 ICBM_Contraption_v1.2.0.155.jar
 ICBM_Explosion_v1.2.0.155.jar
@@ -74,7 +56,6 @@ iChunUtil1.0.1.zip
 immibis-core-55.1.5.jar
 immibis-peripherals-55.0.4.jar
 Improved_Mob_Spawn_1.4.0.zip
-@InfernalMobs_1.5.2.zip
 @invasion_mod_0.11.7.zip
 ironchest-universal-1.5.2-5.2.8.433.zip
 KBI Tinkers Construct Recovery Addon 1.2.1.zip
@@ -89,28 +70,23 @@ miscperipherals-3.3.jar
 ModularPowersuits-0.7.1-559.jar
 More Backpacks 2.1.1.zip
 MPSA-0.3.0-174_MPS-555+.jar
-@MoreGlowstone v1.0 (1.5.2).zip
 Natura_2.1.jar
-Necromancy-1.3-1.5.2.jar
+@Necromancy-1.3-1.5.2.jar
 NetherOres-2.1.5-75.jar
 OmniTools-3.1.6.0.jar
 OpenCCSensors-1.5.2.0.jar
 OpenPeripheral-0.0.7.jar
-@OpenXP-0.0.6.jar
-Panicle_Craft-v1.0.0.4c.zip
-PChan3_mods_0.6(1.5.X).zip
+@Panicle_Craft-v1.0.0.4c.zip
 PluginsforForestry-1.5.2-3.0.17.jar
-PortalGun1.5.2.zip
+@PortalGun1.5.2.zip
 Railcraft_1.5.2-7.2.3.0.jar
-@RancraftPengForge_v152l.zi
 redlogic-55.3.3.jar
 RedstoneInMotion_1.1.0.0_mc1.5.zip
 Rotten_Flesh_To_Leather_MC_1.5.2.zip
 SAP_ManPack_v142.jar
 SecretRoomsMod-universal-4.6.0.283.zip
-@Securecraft 1.1.0.zip
+Securecraft 1.1.0.zip
 SGCraft-0.5.1-mc1.5.1.jar
-@SimplyHorses_1.5.2_pre6.3.zip
 SkullForge.zip
 SlimevoidLib-Universal-v2.0.1.1.zip
 StevesCarts2.0.0.a122.zip
@@ -119,15 +95,12 @@ TConstruct_1.3.6.jar
 ThermalExpansion-2.4.6.0.jar
 @TurretMod_v3.0.1.jar
 UpdateCheckerMod_1.5.2.zip
-@UsefulFood-1.5.1_1.4.2-universal.zip
 Useful_Storage_(5-5)_v.1.8.1.zip
-@Weather_v1.5_Mod_for_MC_v1.5.2.zip
 @WildCaves3-0.4.2.zip
 WirelessRedstone-Universal-v1.7.zip
 WirelessRedstone-PowerConfig-v1.0.zip
 WirelessRedstone-Remote-v2.1.zip
 WirelessRedstone-SlimeVoidAdditions-v1.0.zip
-@Zombie Awareness v1.85 Mod for MC v1.5.2.zip
 [1.5.2]BetterStorage_0.6.1.18.zip
 [1.5.2]bspkrsCorev2.04.zip
 """
