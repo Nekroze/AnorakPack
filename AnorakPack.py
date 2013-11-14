101c5996c7b406cac85ded68d625ad0d7537e506
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-11-14 10:32:28 +1000

    updated mekanism

--- AnorakPack.py
+++ AnorakPack.py
@@ -64,9 +64,9 @@ InfernalMobs_1.6.4.zip
 InventoryTweaks-MC1.6.2-1.56-b77.jar
 @Koi 1.10.4.zip
 mapwriter-2.0.11.zip
-Mekanism-v5.6.0.362.jar
-MekanismGenerators-v5.6.0.362.jar
-MekanismTools-v5.6.0.362.jar
+Mekanism-v5.6.0.372.jar
+MekanismGenerators-v5.6.0.372.jar
+MekanismTools-v5.6.0.372.jar
 MFFS_v3.4.0.232.jar
 MicdoodleCore-1.6.4-2.0.6.895.jar
 Minechem4-7.jar
